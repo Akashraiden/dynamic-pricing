4a137e29f04fba42becc760d0e2e53847294e043
Akashraiden <akasht2098@gmail.com>
2024-12-26 21:34:23 +0530

    data_transformation

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -6,6 +6,7 @@ import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
+
 from sklearn.preprocessing import OneHotEncoder,StandardScaler
 
 from src.exception import CustomException
